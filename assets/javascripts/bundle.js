5def73cd6763086b29f591de9458116418e1edc6
Ellie <elliewawrzaszek@gmail.com>
2017-09-20 10:02:02 -0400

    Add demo modal

--- assets/javascripts/bundle.js
+++ assets/javascripts/bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -70,39 +70,16 @@
 "use strict";
 
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+var _view = __webpack_require__(1);
+
+var _view2 = _interopRequireDefault(_view);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+$(function () {
+  var rootEl = $('.gameboard');
+  new _view2.default(rootEl);
 });
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Board = function () {
-  function Board() {
-    _classCallCheck(this, Board);
-
-    this.grid = Board.setUpGrid();
-  }
-
-  _createClass(Board, null, [{
-    key: "setUpGrid",
-    value: function setUpGrid() {
-      var grid = [];
-      for (var rowIdx = 0; rowIdx < 8; rowIdx++) {
-        grid.push([]);
-        for (var colIdx = 0; colIdx < 8; colIdx++) {
-          grid[rowIdx].push(null);
-        }
-      }
-      return grid;
-    }
-  }]);
-
-  return Board;
-}();
-
-exports.default = Board;
 
 /***/ }),
 /* 1 */
@@ -111,51 +88,19 @@ exports.default = Board;
 "use strict";
 
 
-var _view = __webpack_require__(2);
-
-var _view2 = _interopRequireDefault(_view);
-
-var _game = __webpack_require__(4);
-
-var _game2 = _interopRequireDefault(_game);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-$(function () {
-  var rootEl = $('.gameboard');
-  var game = new _game2.default();
-  new _view2.default(game, rootEl);
-});
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _board = __webpack_require__(0);
-
-var _board2 = _interopRequireDefault(_board);
-
-var _tile = __webpack_require__(3);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var View = function () {
-  function View(game, $el) {
+  function View($el) {
     _classCallCheck(this, View);
 
     this.$el = $el;
-    this.game = game;
 
     this.findTargets = this.findTargets.bind(this);
     this.findMatches = this.findMatches.bind(this);
@@ -163,15 +108,17 @@ var View = function () {
     this.populateBoard = this.populateBoard.bind(this);
     this.checkMatches = this.checkMatches.bind(this);
 
+    // this.modal = this.modal.bind(this);
     this.timer = this.timer.bind(this);
     this.checkHorizontals = this.checkHorizontals.bind(this);
     this.setUpBoard();
 
     this.play();
+    this.modal();
   }
 
   _createClass(View, [{
-    key: 'play',
+    key: "play",
     value: function play() {
       var _this = this;
 
@@ -184,17 +131,24 @@ var View = function () {
       });
     }
   }, {
-    key: 'timer',
+    key: "modal",
+    value: function modal() {
+      $(".modal").click(function () {
+        $('.gamescreen').append("<div class=\"video\">\n          <h1>How to play</h1>\n            <img src=\"./assets/images/slugs_and_dinos_shortened2.gif\" autoplay loop id=\"demo\" />\n            <button onclick=\"$('.video').hide()\">Close</button>\n          </div>");
+      });
+    }
+  }, {
+    key: "timer",
     value: function timer() {
       var elem = document.getElementById("bar");
-      var width = 1;
+      var width = 100;
       var id = setInterval(frame, 500);
       function frame() {
-        if (width >= 100) {
+        if (width <= 0) {
           clearInterval(id);
           $('.gameboard').hide();
           $('.control-panel-column').hide();
-          $('.gamescreen').html('<div class=loser-screen\'><button class=\'newGame\' type=\'button\' onclick=\'location.reload();\'name=\'button\'>New Game</button> <img src=\'assets/images/sad.png\' width=\'500px\'></div>');
+          $('.gamescreen').html("<div class=loser-screen'><button class='newGame' type='button' onclick='location.reload();'name='button'>New Game</button> <img src='assets/images/sad.png' width='500px'></div>");
           var $button = $("<button>");
           $button.addClass("newGame");
         } else {
@@ -204,7 +158,7 @@ var View = function () {
       }
     }
   }, {
-    key: 'checkInside',
+    key: "checkInside",
     value: function checkInside() {
 
       for (var idx = 0; idx < 64; idx++) {
@@ -212,51 +166,51 @@ var View = function () {
         if (borderIndicies.includes(idx)) {
           continue;
         }
-        var $clickedCreature = $('ul.group li:eq(' + idx + ')');
+        var $clickedCreature = $("ul.group li:eq(" + idx + ")");
 
         this.findTargets($clickedCreature);
       }
     }
   }, {
-    key: 'checkHorizontals',
+    key: "checkHorizontals",
     value: function checkHorizontals() {
       var horizontalIndicies = [0, 1, 2, 3, 4, 5, 56, 57, 58, 59, 60, 61];
 
       horizontalIndicies.forEach(function (idx) {
-        var $currentCreature = $('ul.group li:eq(' + idx + ')');
-        var $firstCheck = $('ul.group li:eq(' + (idx + 1) + ')');
-        var $secondCheck = $('ul.group li:eq(' + (idx + 2) + ')');
+        var $currentCreature = $("ul.group li:eq(" + idx + ")");
+        var $firstCheck = $("ul.group li:eq(" + (idx + 1) + ")");
+        var $secondCheck = $("ul.group li:eq(" + (idx + 2) + ")");
 
         if ($currentCreature.html() === $firstCheck.html() && $currentCreature.html() === $secondCheck.html()) {
-          $currentCreature.html("").html('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/bluedino.png height="54px" >');
-          $firstCheck.html("").html('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/slug.png height="54px" >');
-          $secondCheck.html("").html('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/dino2.png height="54px" >');
+          $currentCreature.html("").html("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/bluedino.png height=\"54px\" >");
+          $firstCheck.html("").html("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/slug.png height=\"54px\" >");
+          $secondCheck.html("").html("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/dino2.png height=\"54px\" >");
         }
       });
       return this.checkVerticals();
     }
   }, {
-    key: 'checkVerticals',
+    key: "checkVerticals",
     value: function checkVerticals() {
       var verticalIndicies = [0, 8, 16, 24, 32, 40, 15, 23, 31, 39, 47];
 
       verticalIndicies.forEach(function (idx) {
-        var $currentCreature = $('ul.group li:eq(' + idx + ')');
-        var $firstCheck = $('ul.group li:eq(' + (idx + 8) + ')');
-        var $secondCheck = $('ul.group li:eq(' + (idx + 16) + ')');
+        var $currentCreature = $("ul.group li:eq(" + idx + ")");
+        var $firstCheck = $("ul.group li:eq(" + (idx + 8) + ")");
+        var $secondCheck = $("ul.group li:eq(" + (idx + 16) + ")");
 
         if ($currentCreature.html() === $firstCheck.html() && $currentCreature.html() === $secondCheck.html()) {
           var images = ['dino2.png', 'rex.png', 'bluedino.png', 'slug1.png', 'slug.png'];
           // const newHTML = `<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/${images[(Math.floor(Math.random() * 5)) % 5]} height="54px" >`
-          $currentCreature.html("").html('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/dino2.png height="54px" >');
-          $firstCheck.html("").html('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/slug1.png height="54px" >');
-          $secondCheck.html("").html('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/rex.png height="54px" >');
+          $currentCreature.html("").html("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/dino2.png height=\"54px\" >");
+          $firstCheck.html("").html("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/slug1.png height=\"54px\" >");
+          $secondCheck.html("").html("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/rex.png height=\"54px\" >");
         }
       });
       return this.checkInside();
     }
   }, {
-    key: 'findTargets',
+    key: "findTargets",
     value: function findTargets($clickedCreature) {
       var clickedCreatureIdx = $clickedCreature.index();
       var deltas = [-9, -8, -7, -1, 1, 7, 8, 9];
@@ -271,14 +225,14 @@ var View = function () {
       return this.findMatches(possibleTargets, $clickedCreature);
     }
   }, {
-    key: 'findMatches',
+    key: "findMatches",
     value: function findMatches(possibleTargets, $clickedCreature) {
       var matches = [];
       possibleTargets.forEach(function (index) {
 
-        if ($clickedCreature.html() === $('ul.group li:eq(' + index + ')').html()) {
+        if ($clickedCreature.html() === $("ul.group li:eq(" + index + ")").html()) {
           // $( `ul.group li:eq(${index})`).css("background-color", "red");
-          matches.push($('ul.group li:eq(' + index + ')'));
+          matches.push($("ul.group li:eq(" + index + ")"));
           // matches.push(index);
           console.log(matches);
           console.log($(matches));
@@ -293,23 +247,23 @@ var View = function () {
       }
     }
   }, {
-    key: 'checkMatches',
+    key: "checkMatches",
     value: function checkMatches(matches, $clickedCreature) {
       matches.forEach(function (match) {
         var diff = $clickedCreature.index() - $(match).index();
         var possibleThirdIdx = $(match).index() - diff;
-        var possibleThird = $('ul.group li:eq(' + possibleThirdIdx + ')');
+        var possibleThird = $("ul.group li:eq(" + possibleThirdIdx + ")");
         if ($(match).html() === $(possibleThird).html()) {
           var images = ['dino2.png', 'rex.png', 'slug.png', 'slug1.png', 'bluedino.png'];
-          var newHTML = '<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/' + images[Math.floor(Math.random() * 5) % 5] + ' height="54px" >';
-          possibleThird.html("").html('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/' + images[Math.floor(Math.random() * 5) % 5] + ' height="54px" >');
-          $(match).html("").html('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/' + images[Math.floor(Math.random() * 5) % 5] + ' height="54px" >');
-          $clickedCreature.html("").html('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/' + images[Math.floor(Math.random() * 5) % 5] + ' height="54px" >');
+          var newHTML = "<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/" + images[Math.floor(Math.random() * 5) % 5] + " height=\"54px\" >";
+          possibleThird.html("").html("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/" + images[Math.floor(Math.random() * 5) % 5] + " height=\"54px\" >");
+          $(match).html("").html("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/" + images[Math.floor(Math.random() * 5) % 5] + " height=\"54px\" >");
+          $clickedCreature.html("").html("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/" + images[Math.floor(Math.random() * 5) % 5] + " height=\"54px\" >");
         }
       });
     }
   }, {
-    key: 'setUpBoard',
+    key: "setUpBoard",
     value: function setUpBoard() {
       var $ul = $("<ul>");
       $ul.addClass("group");
@@ -329,11 +283,11 @@ var View = function () {
       this.$el.append($ul);
     }
   }, {
-    key: 'populateBoard',
+    key: "populateBoard",
     value: function populateBoard() {
       var images = ['bluedino.png', 'slug.png', 'slug1.png', 'rex.png', 'dino2.png'];
       $("ul li").each(function (index) {
-        $(this).append('<img draggable="true" ondrop="drop_handler(event);" ondragover="dragover_handler(event);" ondragstart="dragstart_handler(event);" src=assets/images/' + images[(index + Math.floor(Math.random() * 6)) % 5] + ' height="54px" >');
+        $(this).append("<img draggable=\"true\" ondrop=\"drop_handler(event);\" ondragover=\"dragover_handler(event);\" ondragstart=\"dragstart_handler(event);\" src=assets/images/" + images[(index + Math.floor(Math.random() * 6)) % 5] + " height=\"54px\" >");
       }).addClass('icon');
       this.checkHorizontals();
     }
@@ -343,41 +297,6 @@ var View = function () {
 }();
 
 exports.default = View;
-
-/***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-/***/ }),
-/* 4 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _board = __webpack_require__(0);
-
-var _board2 = _interopRequireDefault(_board);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Game = function Game() {
-  _classCallCheck(this, Game);
-
-  this.board = new _board2.default();
-  // this.countScore = this.countScore.bind(this)
-};
-
-exports.default = Game;
 
 /***/ })
 /******/ ]);
